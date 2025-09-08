8f922509fccf176f157afa52933db59442155e81
BrookSaranyapong <Brook.saranyapong@gmail.com>
2025-09-08 21:17:44 +0700

    "update"

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import LayoutAdmin from '@/layouts/LayoutAdmin';
+
+export default function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            <LayoutAdmin>
+                <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+                    <div className="lg:col-span-2">{children}</div>
+                </div>
+            </LayoutAdmin>
+        </div>
+    );
+}
